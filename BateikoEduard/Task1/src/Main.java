a8401231d34ffbc1b1e592fcbd5c119a186fae00
bateiko <bateiko0713@gmail.com>
2019-12-08 21:34:47 +0200

    steated Task1

--- BateikoEduard/Task1/src/Main.java
+++ BateikoEduard/Task1/src/Main.java
@@ -1,5 +1,10 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        
+        List<Integer> ArrList = new ArrayList<>();
+        List<Integer> ArrLinkedList = new LinkedList<>();
+        AbstractSet<Integer> ArrTreeSet = new TreeSet<>();
+        AbstractSet<Integer> ArrHashSet = new HashSet<>();
     }
 }
